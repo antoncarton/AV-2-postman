0be33da6ad6dddf0b948966bded99f72ab39d16b
antoncarton <aksalutly@gmail.com>
2022-09-10 14:54:50 +0500

    wrong test

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+    @Test
+    void PostmanEchoTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("name",equalTo("some data"));
+        ;
+    }
+}
